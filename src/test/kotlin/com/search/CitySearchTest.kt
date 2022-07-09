1df1c7e60afea7f6aba21573ac3e045f4ce6914c
e.shoghi@fwutech.com <e.shoghi@fwutech.com>
2022-07-09 17:56:40 +0430

    Create the test case to check that if search text is fewer than 2 characters, no results return.

--- src/test/kotlin/com/search/CitySearchTest.kt
+++ src/test/kotlin/com/search/CitySearchTest.kt
@@ -1,4 +1,11 @@
 package com.search
 
+import org.junit.Test
+import kotlin.test.assertNull
+
 class CitySearchTest {
+    @Test
+    fun `should return no results for search text is fewer than 2 characters`() {
+        assertNull(CitySearch().search("l"))
+    }
 }
