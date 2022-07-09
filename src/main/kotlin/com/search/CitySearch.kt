278497164e501f91d604939791622c08dc445ba0
e.shoghi@fwutech.com <e.shoghi@fwutech.com>
2022-07-09 18:16:54 +0430

    Pass the test.

--- src/main/kotlin/com/search/CitySearch.kt
+++ src/main/kotlin/com/search/CitySearch.kt
@@ -1,8 +1,11 @@
 package com.search
 
 class CitySearch {
-    fun search(searchText: String): String? {
-        return if (searchText.length < 2) null else searchText
+    val cities = listOf("Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna",
+            "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul")
+
+    fun search(searchText: String): List<String> {
+        return if (searchText.length < 2) listOf() else cities.filter { it.startsWith(searchText) }
     }
 
 }
