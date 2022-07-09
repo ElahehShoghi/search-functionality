fd18a3b7bbb88d125a16321b895aa4674a5bd43f
e.shoghi@fwutech.com <e.shoghi@fwutech.com>
2022-07-09 18:40:48 +0430

    Refactor

--- src/main/kotlin/com/search/CitySearch.kt
+++ src/main/kotlin/com/search/CitySearch.kt
@@ -1,13 +1,11 @@
 package com.search
 
 class CitySearch {
-    private val cities = listOf("Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna",
-            "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul")
+    private val cities = listOf("Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul")
 
     fun search(searchText: String): List<String> {
-        if (searchText == "*")
-            return cities
-        return if (searchText.length < 2) listOf() else cities.filter { it.contains(searchText, ignoreCase = true) }
+        return if (searchText == "*") cities else if (searchText.length < 2) listOf()
+        else cities.filter { it.contains(searchText, ignoreCase = true) }
     }
 
 }
