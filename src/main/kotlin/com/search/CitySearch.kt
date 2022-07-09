49004fbb45c7f8ac841563a119855f9b7d8841c0
e.shoghi@fwutech.com <e.shoghi@fwutech.com>
2022-07-09 18:36:39 +0430

    Pass the test.

--- src/main/kotlin/com/search/CitySearch.kt
+++ src/main/kotlin/com/search/CitySearch.kt
@@ -5,6 +5,8 @@ class CitySearch {
             "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul")
 
     fun search(searchText: String): List<String> {
+        if (searchText == "*")
+            return cities
         return if (searchText.length < 2) listOf() else cities.filter { it.contains(searchText, ignoreCase = true) }
     }
 
