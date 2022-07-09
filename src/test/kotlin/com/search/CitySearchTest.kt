6b385f780c7fc381bd72ac5f2ff6df6b1f129774
e.shoghi@fwutech.com <e.shoghi@fwutech.com>
2022-07-09 18:13:01 +0430

    Create the test case to check that if search text is more than 1 character, city names starting with that returns.

--- src/test/kotlin/com/search/CitySearchTest.kt
+++ src/test/kotlin/com/search/CitySearchTest.kt
@@ -1,11 +1,17 @@
 package com.search
 
 import org.junit.Test
-import kotlin.test.assertNull
+import kotlin.test.assertEquals
 
 class CitySearchTest {
+
     @Test
     fun `should return no results for search text is fewer than 2 characters`() {
-        assertNull(CitySearch().search("l"))
+        assertEquals(CitySearch().search("l"), listOf())
+    }
+
+    @Test
+    fun `should return city names starting with the search text which are more than 1 character`() {
+        assertEquals(listOf("Valencia", "Vancouver"), CitySearch().search("Va"))
     }
 }
